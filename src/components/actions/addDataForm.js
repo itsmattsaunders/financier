d177133b6dea477154c9894ce59c9e30d85c43da
itsmattsaunders <itsmattsaunders@gmail.com>
2024-01-03 12:21:05 +1300

    remove check, didn't really like it much anyway

--- src/components/actions/addDataForm.js
+++ src/components/actions/addDataForm.js
@@ -92,17 +92,6 @@ export default function AddDataForm({ isOpen: { isOpen }, setIsOpen }) {
                         <span className={classNames(selected ? 'font-semibold' : 'font-normal', 'block truncate')}>
                           {selection.name}
                         </span>
-
-                        {selected ? (
-                          <span
-                            className={classNames(
-                              active ? 'text-white' : 'text-indigo-600',
-                              'absolute inset-y-0 right-0 flex items-center pr-4'
-                            )}
-                          >
-                            <CheckIcon className="h-5 w-5" aria-hidden="true" />
-                          </span>
-                        ) : null}
                       </>
                     )}
                   </Listbox.Option>
