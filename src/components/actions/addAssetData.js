286ebebc5327aa7ef017c24e2070e99da48b824e
itsmattsaunders <itsmattsaunders@gmail.com>
2024-01-03 12:13:36 +1300

    change to camel and not snake case

--- src/components/actions/addAssetData.js
+++ src/components/actions/addAssetData.js
@@ -49,15 +49,15 @@ export default function AddAssetData({
 
             <div className="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
             <div className="sm:col-span-3">
-                <label htmlFor="asset-name" className="block text-sm font-medium leading-6 text-gray-900">
+                <label htmlFor="assetName" className="block text-sm font-medium leading-6 text-gray-900">
                 Asset Name
                 </label>
                 <input
                 onChange={handleNameChange}
                 value={name}
                 type="text"
-                name="asset-name"
-                id="asset-name"
+                name="assetName"
+                id="assetName"
                 required
                 className="mt-2 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                 placeholder="Asset name"
@@ -65,7 +65,7 @@ export default function AddAssetData({
             </div>
 
             <div className="sm:col-span-3">
-                <label htmlFor="asset-value" className="block text-sm font-medium leading-6 text-gray-900">
+                <label htmlFor="assetValue" className="block text-sm font-medium leading-6 text-gray-900">
                 Value ($)
                 </label>
                 <input
@@ -73,8 +73,8 @@ export default function AddAssetData({
                 value={value}
                 type="text"
                 inputMode="decimal"
-                name="asset-value"
-                id="asset-value"
+                name="assetValue"
+                id="assetValue"
                 pattern="^-?[0-9]\d*(\.\d+)?$"
                 required
                 className="mt-2 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
@@ -83,7 +83,7 @@ export default function AddAssetData({
             </div>
 
             <div className="sm:col-span-3">
-                <label htmlFor="interest-rate" className="block text-sm font-medium leading-6 text-gray-900">
+                <label htmlFor="interestRate" className="block text-sm font-medium leading-6 text-gray-900">
                 Interest/Return Rate (%)
                 </label>
                 <input
@@ -91,9 +91,9 @@ export default function AddAssetData({
                 value={rate}
                 type="text"
                 inputMode="decimal"
-                name="interest-rate"
-                id="interest-rate"
-                pattern="^-?[1-9]\d*(\.\d+)?$"
+                name="interestRate"
+                id="interestRate"
+                pattern="^-?[0-9]\d*(\.\d+)?$"
                 required
                 className="mt-2 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                 placeholder="5.5"
@@ -101,54 +101,54 @@ export default function AddAssetData({
             </div>
 
             <div className="sm:col-span-3">
-                <label htmlFor="start-date" className="block text-sm font-medium leading-6 text-gray-900">
+                <label htmlFor="startDate" className="block text-sm font-medium leading-6 text-gray-900">
                 Start Date (optional)
                 </label>
                 <input
                 onChange={handleStartDateChange}
                 value={startDate}
                 type="date"
-                name="start-date"
-                id="start-date"
+                name="startDate"
+                id="startDate"
                 className="mt-2 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                 />
             </div>
 
             <div className="sm:col-span-3">
-                <label htmlFor="end-date" className="block text-sm font-medium leading-6 text-gray-900">
+                <label htmlFor="endDate" className="block text-sm font-medium leading-6 text-gray-900">
                 End Date (optional)
                 </label>
                 <input
                 onChange={handleEndDateChange}
                 type="date"
                 value={endDate}
-                name="end-date"
-                id="end-date"
+                name="endDate"
+                id="endDate"
                 className="mt-2 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                 />
             </div>
             <div className="sm:col-span-3"></div>
                 <div className="col-span-full">
-                    <label htmlFor="asset-type" className="block text-sm font-medium leading-6 text-gray-900 mb-3">Asset Type</label>
+                    <label htmlFor="assetType" className="block text-sm font-medium leading-6 text-gray-900 mb-3">Asset Type</label>
                     <div className="flex flex-wrap">
                         <div className="flex items-center space-x-2 mb-3 w-6/12">
-                            <input value={type} onChange={handleTypeChange} required type="radio" id="savings" name="asset-type" value="savings" className="h-4 w-4 text-indigo-600 border-gray-300 focus:ring-indigo-500" />
+                            <input checked={type === "savings"} value="savings" onChange={handleTypeChange} required type="radio" id="savings" name="assetType" className="h-4 w-4 text-indigo-600 border-gray-300 focus:ring-indigo-500" />
                             <label htmlFor="savings" className="min-w-0 flex-1 text-sm font-medium text-gray-700">Savings</label>
                         </div>
                         <div className="flex items-center space-x-2 mb-3 w-6/12">
-                            <input value={type} onChange={handleTypeChange} required type="radio" id="stocks" name="asset-type" value="stocks" className="h-4 w-4 text-indigo-600 border-gray-300 focus:ring-indigo-500" />
+                            <input checked={type === "stocks"} value="stocks" onChange={handleTypeChange} required type="radio" id="stocks" name="assetType" className="h-4 w-4 text-indigo-600 border-gray-300 focus:ring-indigo-500" />
                             <label htmlFor="stocks" className="min-w-0 flex-1 text-sm font-medium text-gray-700">Stocks (including ETFs)</label>
                         </div>
                         <div className="flex items-center space-x-2 mb-3 w-6/12">
-                            <input value={type} onChange={handleTypeChange} required type="radio" id="termDeposits" name="asset-type" value="termDeposits" className="h-4 w-4 text-indigo-600 border-gray-300 focus:ring-indigo-500" />
+                            <input checked={type === "termDeposits"} value="termDeposits" onChange={handleTypeChange} required type="radio" id="termDeposits" name="assetType" className="h-4 w-4 text-indigo-600 border-gray-300 focus:ring-indigo-500" />
                             <label htmlFor="termDeposits" className="min-w-0 flex-1 text-sm font-medium text-gray-700">Term Deposits</label>
                         </div>
                         <div className="flex items-center space-x-2 mb-3 w-6/12">
-                            <input value={type} onChange={handleTypeChange} required type="radio" id="crypto" name="asset-type" value="crypto" className="h-4 w-4 text-indigo-600 border-gray-300 focus:ring-indigo-500" />
+                            <input checked={type === "crypto"} value="crypto" onChange={handleTypeChange} required type="radio" id="crypto" name="assetType" className="h-4 w-4 text-indigo-600 border-gray-300 focus:ring-indigo-500" />
                             <label htmlFor="crypto" className="min-w-0 flex-1 text-sm font-medium text-gray-700">Crypto</label>
                         </div>
                         <div className="flex items-center space-x-2 mb-3 w-6/12">
-                            <input value={type} onChange={handleTypeChange} required type="radio" id="other" name="asset-type" value="other" className="h-4 w-4 text-indigo-600 border-gray-300 focus:ring-indigo-500" />
+                            <input checked={type === "other"} value="other" onChange={handleTypeChange} required type="radio" id="other" name="assetType" className="h-4 w-4 text-indigo-600 border-gray-300 focus:ring-indigo-500" />
                             <label htmlFor="other" className="min-w-0 flex-1 text-sm font-medium text-gray-700">Other (property, etc)</label>
                         </div>
                     </div>
