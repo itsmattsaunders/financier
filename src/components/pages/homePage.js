eaa5c2812a746946a0c902ea90da17bbd0ba9f67
itsmattsaunders <itsmattsaunders@gmail.com>
2024-01-02 15:17:05 +1300

    added some copy for home page

--- src/components/pages/homePage.js
+++ src/components/pages/homePage.js
@@ -2,13 +2,14 @@ import React from 'react';
 
 export default function HomePage() {
     return (
-    <div className="bg-white px-6 py-24 sm:py-32 lg:px-8">
+    <div className="bg-white px-6 py-24 sm:py-32 lg:px-9">
         <div className="mx-auto max-w-2xl text-center">
-          <p className="text-base font-semibold leading-7 text-indigo-600">Get the help you need</p>
-          <h2 className="mt-2 text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">Support center</h2>
+          <p className="text-base font-semibold leading-7 text-indigo-700">
+            Elevate your personal finance journey with Financier.
+          </p>
+          <h2 className="mt-2 text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">Navigate Your Finances with Financier</h2>
           <p className="mt-6 text-lg leading-8 text-gray-600">
-            Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem cupidatat commodo. Elit sunt amet
-            fugiat veniam occaecat fugiat aliqua.
+            Discover Financier, your personal finance web app powered by OpenAI, designed to illuminate and guide your financial journey. As your digital compass, it offers clear insights and empowers you to make informed decisions. With Financier, steer your own course to financial success with confidence and ease. Join now and take control of your financial future.
           </p>
         </div>
       </div>
