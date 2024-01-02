86bcf9b58c59afe19f4b5a7318169c5283eaeb4c
itsmattsaunders <itsmattsaunders@gmail.com>
2024-01-03 12:18:21 +1300

    succesfully updates financial context with new asset with no bugs found

--- src/components/actions/addDataForm.js
+++ src/components/actions/addDataForm.js
@@ -9,41 +9,57 @@ function classNames(...classes) {
 }
 
 export default function AddDataForm({ isOpen: { isOpen }, setIsOpen }) {
-  const selections = [{ id: 1, type: 'add-expense', name: 'Add Expense' }, { id: 2, type: 'add-asset', name: 'Add Asset' }];
-  const [selected, setSelected] = useState(selections[1]);
+  const selections = [{ id: 1, type: 'assets', name: 'Add Asset' }, { id: 2, type: 'expense', name: 'Add Expense' }, { id: 3, type: 'income', name: 'Add Income' }];
+  const [selected, setSelected] = useState(selections[0]);
   const { updateFinancialData } = useContext(FinancialContext);
 
   // Form States
   const [name, setName] = useState('');
-  const [value, setValue] = useState(0);
-  const [rate, setRate] = useState(1);
+  const [value, setValue] = useState('');
+  const [rate, setRate] = useState('');
   const [startDate, setStartDate] = useState('');
   const [endDate, setEndDate] = useState('');
-  const [type, setType] = useState('');
-  const [income, setIncome] = useState(0);
-  const [expense, setExpense] = useState(0);
+  const [type, setType] = useState('savings');
+  const [income, setIncome] = useState('');
+  const [expense, setExpense] = useState('');
 
   const handleFormSubmit = (e) => {
     e.preventDefault();
     const formData = new FormData(e.target);
     const formProps = Object.fromEntries(formData);
+    const randomId = Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15);
     const newAsset = {
-      timestamp: new Date().toISOString(),
-      ...formProps
+      ID: randomId,
+      ...formProps,
+      timeStamp: new Date().toISOString()
     };
 
-    console.log(newAsset)
+    const type = selected.type;
 
-    updateFinancialData(newAsset, "assets");
+    updateFinancialData(newAsset, type);
+    setName('');
+    setValue('');
+    setRate('');
+    setStartDate('');
+    setEndDate('');
+    setType('savings');
+    setIncome('');
+    setExpense('');
+    setSelected(selections[0]);
   };
 
+  const handleSelectionChange = (selectedOption) => {
+    const selection = selections.filter(selection => selectedOption === selection.type);
+    setSelected(selection[0]);
+  };
+  
   return (
     <form onSubmit={handleFormSubmit}>
     {/* TODO: Don't love this but maybe I can change it later?? */}
-    <Listbox value={selected} onChange={setSelected}>
+    <Listbox value={selected} onChange={handleSelectionChange}>
       {({ open }) => (
         <>
-          <Listbox.Label className="block text-sm font-medium leading-6 text-gray-900">Assigned to</Listbox.Label>
+          <Listbox.Label className="block text-sm font-medium leading-6 text-gray-900">Add Financial Data</Listbox.Label>
           <div className="relative mt-2">
             <Listbox.Button className="relative w-full cursor-default rounded-md bg-white py-1.5 pl-3 pr-10 text-left text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:outline-none focus:ring-2 focus:ring-indigo-600 sm:text-sm sm:leading-6">
               <span className="block truncate">{selected.name}</span>
@@ -60,21 +76,21 @@ export default function AddDataForm({ isOpen: { isOpen }, setIsOpen }) {
               leaveTo="opacity-0"
             >
               <Listbox.Options className="absolute z-10 mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm">
-                {selections.map((person) => (
+                {selections.map((selection) => (
                   <Listbox.Option
-                    key={person.id}
+                    key={selection.id}
                     className={({ active }) =>
                       classNames(
                         active ? 'bg-indigo-600 text-white' : 'text-gray-900',
                         'relative cursor-default select-none py-2 pl-3 pr-9'
                       )
                     }
-                    value={person}
+                    value={selection.type}
                   >
                     {({ selected, active }) => (
                       <>
                         <span className={classNames(selected ? 'font-semibold' : 'font-normal', 'block truncate')}>
-                          {person.name}
+                          {selection.name}
                         </span>
 
                         {selected ? (
@@ -98,7 +114,7 @@ export default function AddDataForm({ isOpen: { isOpen }, setIsOpen }) {
       )}
     </Listbox>
 
-    {selected.type === 'add-asset' ? 
+    {selected.type === 'assets' ? 
       <AddAssetData   
         name={name}
         setName={setName}
