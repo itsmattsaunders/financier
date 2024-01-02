2d02dbd1fb8ce7b8bdf5e7f7faaf64c42722e6c6
itsmattsaunders <itsmattsaunders@gmail.com>
2024-01-02 23:32:52 +1300

    created initial state and updated the update function

--- src/financialContext.js
+++ src/financialContext.js
@@ -1,12 +1,28 @@
 import React, { createContext, useState } from 'react';
 
+const initialState = {
+  "financialOverview": {
+    "income": [],
+    "assets": [],
+    "liabilities": [],
+    "financialScoreNew": {},
+    "financialScoreOld": {}
+  }
+}
+
 export const FinancialContext = createContext();
 
 export function FinancialProvider({ children }) {
-    const [financialData, setFinancialData] = useState({});
+    const [financialData, setFinancialData] = useState(initialState);
 
-    const updateFinancialData = (newData) => {
-        setFinancialData((prevData) => ({ ...prevData, ...newData }));
+    const updateFinancialData = (newData, category) => {
+        setFinancialData(prevState => ({
+            ...prevState,
+            financialOverview: {
+              ...prevState.financialOverview,
+              [category]: [...prevState.financialOverview[category], newData] 
+            }
+        }));
     };
 
     return (
