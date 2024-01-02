3a0618582324aa6097dd9a8b1aeca4bc4709ebf5
itsmattsaunders <itsmattsaunders@gmail.com>
2024-01-02 23:31:47 +1300

    added a better name for rotue

--- src/components/navigation/nav.js
+++ src/components/navigation/nav.js
@@ -63,13 +63,13 @@ export default function Nav() {
                     </li>
                     <li>
                         <NavLink 
-                            to="/liabilities"
+                            to="/liabilities-and-debts"
                             className={({ isActive }) =>
                             isActive
                               ? "rounded-md bg-gray-900 px-3 py-2 text-sm font-medium text-white"
                               : "rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white"
                           }>
-                            Liabilities
+                            Liabilities & Debts
                         </NavLink>
                     </li>
                   </ul>
